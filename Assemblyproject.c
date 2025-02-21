954e338cb92f92f170593f9836923f1afd66bf8c
RagingRodent <rmuhamend@kheprw.org>
2025-02-20 23:00:50 -0500

    new proj

---
+++ Assemblyproject.c
@@ -0,0 +1,29 @@
+/*
+ * This is a boilerplate C program that provides a starting point for
+ * developing C programs. It includes a basic main function and some
+ * useful comments to help you get started.
+ *
+ * Author: [Your Name]
+ * Date: [Today's Date]
+ */
+
+#include <stdio.h>
+
+int main(void) {
+    // This is the main function of the program
+    printf("Hello, World!\n");
+
+    return 0;
+}
+
+/*
+ * The main function is the entry point for the program.
+ * It returns an integer value to the operating system.
+ *
+ * The `printf` function is used to print text to the console.
+ * In this case, it prints the message "Hello, World!" followed
+ * by a newline character.
+ *
+ * The `return 0` statement indicates that the program has completed
+ * successfully. A non-zero return value typically indicates an error.
+ */
